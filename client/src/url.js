422e1cbc7e3cdf526340608552d75a18f33ef13b
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-01-17 22:00:24 +0530

    Update URL for production environment

--- client/src/url.js
+++ client/src/url.js
@@ -3,7 +3,7 @@ let url;
 if (window.location.hostname === 'localhost') {
     url = 'http://localhost:4999';
 } else if (window.location.hostname === 'cropmate.onrender.com') {
-    url = 'https://cropmate.onrender.com';
+    url = 'https://farm-stack-ai-868q.vercel.app';
 } else {
     // Default URL
     url = 'http://localhost:4999';
