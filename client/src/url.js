f15509a024695565e21e386a16927b626d1d476b
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-01-17 22:26:01 +0530

    Update url.js

--- client/src/url.js
+++ client/src/url.js
@@ -1,12 +1,12 @@
 let url;
 
-if (window.location.hostname === 'localhost') {
-    url = 'http://localhost:4999';
-} else if (window.location.hostname === 'farm-stack-ai.vercel.app/') {
-    url = 'https://farm-stack-ai-868q.vercel.app';
+if (window.location.hostname === "localhost") {
+  url = "http://localhost:4999";
+} else if (window.location.hostname === "farm-stack-ai.vercel.app") {
+  url = "https://farm-stack-ai-868q.vercel.app";
 } else {
-    // Default URL
-    url = 'http://localhost:4999';
+  url = "https://farm-stack-ai-868q.vercel.app"; // safer default
 }
 
 export default url;
+
