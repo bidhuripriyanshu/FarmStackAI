e942b8f77726686e6b9e8290ef948369d783818c
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-01-17 22:21:50 +0530

    Update url.js

--- client/src/url.js
+++ client/src/url.js
@@ -2,7 +2,7 @@ let url;
 
 if (window.location.hostname === 'localhost') {
     url = 'http://localhost:4999';
-} else if (window.location.hostname === 'cropmate.onrender.com') {
+} else if (window.location.hostname === 'farm-stack-ai.vercel.app/') {
     url = 'https://farm-stack-ai-868q.vercel.app';
 } else {
     // Default URL
